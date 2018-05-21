808dc2c690d4ded06d9d361ae9afff45a41912ae
Rubtsov Sergey <rsg81@ya.ru>
2018-05-21 21:12:42 +0300

    Получили список

--- rxlsx.py
+++ rxlsx.py
@@ -16,20 +16,28 @@ last_sheet = excel_data_file.nsheets
 sheet = excel_data_file.sheet_by_index(last_sheet - 1)
 
 # Получаем данные из последнего листа
-vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows) ]
+vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
 
 # Удаляем пустые значения в списке
 vals2 = []
 for i in range(len(vals)):
     vals2.append([])
     for el in vals[i]:
-          if el != '': vals2[i].append(el)
+        if el != '': vals2[i].append(el)
 
 # Удаляем пустые вложенные списки
 vals = [value for value in vals2 if value]
 
 # Удаляем не нужные элементы
-del vals2
 del vals[-3:]
 del vals[:1]
+
+# Избавляемся от вложенных списков
+vals2 = []
+for i in vals:
+    for j in i:
+        vals2.append(j)
+vals = vals2
+del vals2
+
 print(vals)
